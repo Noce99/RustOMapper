34327b33a2f54433f25a8eb8e1dfd868b724cc10
Noce99 <enrico.mannocci99@gmail.com>
2025-07-09 16:02:13 +0200

    Studing the code!

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,12 @@ mod map;
 use crate::map_file::reading::read_o_mapper_file;
 
 fn main() {
-    let a_map = read_o_mapper_file("/");
+    let a_map = read_o_mapper_file("Maps/emptymap.omap");
+
+    print!("{}", a_map);
+
+    let struct_types = a_map.get_struct_types();
+    for struct_type in &struct_types{
+        println!("{}", struct_type);
+    }
 }
--- src/map/symbols.rs
+++ src/map/symbols.rs
@@ -7,6 +7,10 @@ pub trait Symbol{
 }
 
 pub struct SymbolsBag {
+    // There we are creating a Vector of Box.
+    // A Box is simply a container that store the data on the heap instead that on the stack, when
+    // the owner change just the pointer in te stack is copied and not the real data on the heap.
+    // dyn Symbol means that inside the Boxes we can have any struct that implement the Symbol trait
     bag: Vec<Box<dyn Symbol>>
 }
 
